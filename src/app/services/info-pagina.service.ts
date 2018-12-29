4c11199495a78043558961d8cb6862f1b118da9e
Alex <alexxsur@gmail.com>
2018-12-29 17:54:03 -0500

    Cargar información de Firebase en la página

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,17 +10,32 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
-  constructor(private http: HttpClient) {
-    // console.log('Servicio de infoPagina listo');
+  equipo: any[] = [];
 
+  constructor(private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
     // Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
     .subscribe( (resp: InfoPagina) => {
 
       this.cargada = true;
       this.info = resp;
+    });
+  }
+
+  private cargarEquipo() {
+    // Leer el archivo JSON
+    this.http.get('https://curso-angular-html.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+
+      this.equipo = resp;
       console.log(resp);
 
     });
   }
+
 }
